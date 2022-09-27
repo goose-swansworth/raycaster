345228b8b93574f52a3f1cc50ec7b235e506036f
Keetley Rate <kjr106@uclive.ac.nz>
2022-09-28 01:04:17 +1300

    square rendering, wowee

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,92 @@
+use glium::glutin::platform::unix::WindowBuilderExtUnix;
+
 extern crate glium;
 
 fn main() {
+    use glium::glutin::dpi::PhysicalSize;
+    use glium::implement_vertex;
     use glium::{glutin, Surface};
 
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_base_size(PhysicalSize::new(1600, 900))
+        .with_title("~Raycaster~");
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-    
+
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex {
+        position: [0.05, -0.05 * (16.0 / 9.0)],
+    };
+    let vertex2 = Vertex {
+        position: [0.05, 0.05 * (16.0 / 9.0)],
+    };
+    let vertex3 = Vertex {
+        position: [-0.05, -0.05 * (16.0 / 9.0)],
+    };
+    let vertex4 = Vertex {
+        position: [-0.05, 0.05 * (16.0 / 9.0)],
+    };
+
+    let shape = vec![vertex1, vertex2, vertex3, vertex4];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
+
     event_loop.run(move |ev, _, control_flow| {
-        
         let mut target = display.draw();
-        target.clear_color(0.7, 0.2, 0.3, 1.0);
+        target.clear_color(0.2, 0.2, 0.2, 1.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
         target.finish().unwrap();
-        
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
         match ev {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
-                },
+                }
                 _ => return,
             },
             _ => (),
